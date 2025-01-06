1094ddad43d3484247c802032acc715c8c6d4e44
itaym19 <openu1111@gmail.com>
2025-01-06 21:13:02 +0200

    Update complex.h

--- complex.h
+++ complex.h
@@ -1,2 +1,7 @@
 typedef double complex[2];
+void read_comp (complex * c, double a, double b);
+void print_comp (complex c);
+void add_comp (complex a, complex b);
+void sub_comp (complex a, complex b);
+
 
