896157149c0f677ad0c759c98ec8f696f324ab2d
itaym19 <openu1111@gmail.com>
2025-01-07 17:15:17 +0200

    Update complex.c

--- complex.c
+++ complex.c
@@ -1,2 +1,12 @@
 #include <stdio.h>
 #include <math.h>
+#include "complex.h"
+
+void read_comp (complex *c, double a, double b){
+    (*c)[0] = a;
+    (*c)[1] = b;
+}
+
+void print_comp (complex c){
+    printf("%.2lf + (%.2lf)i", c[0], c[1] );
+}
