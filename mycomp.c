340bb90fc7285b2f7bd656c7a1035a147c3daf55
itaym19 <openu1111@gmail.com>
2025-01-08 22:18:02 +0200

    Update mycomp.c

--- mycomp.c
+++ mycomp.c
@@ -14,8 +14,14 @@ int main()
     print_comp(A);
     printf("\nB value:\n");
     print_comp(B);
-    printf("\nA+B value:\n");
+    printf("\nA + B value:\n");
     add_comp(A,B);
+    printf("\nA - B value:\n");
+    sub_comp(A,B);
+    printf("\nenter real values to multuply A\n");
+    scanf("%lf", &real);
+    printf("\nA * %.2lf value:\n", real);
+    mult_comp_real(A, real);
 
     return 0;
 }
