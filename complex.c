38ecf364113d43a537043c15050519503790f488
itaym19 <openu1111@gmail.com>
2025-01-08 10:10:47 +0200

    Update complex.c

--- complex.c
+++ complex.c
@@ -10,3 +10,12 @@ void read_comp (complex *c, double a, double b){
 void print_comp (complex c){
     printf("%.2lf + (%.2lf)i\n", c[0], c[1] );
 }
+
+void add_comp (complex a, complex b){
+    double real, img;
+    complex c;
+    real = a[0] + b[0];
+    img = a[1] +b[1];
+    read_comp(&c, real, img);
+    print_comp(c);
+}
