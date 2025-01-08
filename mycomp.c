eaa6bc7dc3ac830f8c6fc1a7c63e8fd576e29699
itaym19 <openu1111@gmail.com>
2025-01-08 17:05:10 +0200

    Update mycomp.c

--- mycomp.c
+++ mycomp.c
@@ -5,10 +5,10 @@ int main()
     double real, img;
     complex A,B,C,D,E,F;
     printf("enter real and img values for A\n");
-    scanf("%lf, %lf", &real,&img);
+    scanf("%lf %lf", &real,&img);
     read_comp(&A, real, img);
     printf("enter real and img values for B\n");
-    scanf("%lf, %lf", &real,&img);
+    scanf("%lf %lf", &real,&img);
     read_comp(&B, real, img);
     printf("\nA value:\n");
     print_comp(A);
